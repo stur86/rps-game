4b314d0cd8b50f7c9a6be030bc06c1868be18ad1
Simone <simonesturniolo@gmail.com>
2024-08-12 14:44:12 +0100

    feat: Add score tracking to CLI game

--- game.ts
+++ game.ts
@@ -7,8 +7,10 @@ const debug = process.argv[3] === 'debug';
 const ai = new RPSLinearNGramCombo(n);
 
 const moves = [RPSMove.ROCK, RPSMove.PAPER, RPSMove.SCISSORS];
-// Read from stdin
+const score = [0, 0, 0]; // Wins, ties, losses
+
 process.stdout.write('Enter your move [R, P, S]: ');
+// Read from stdin
 for await (const line of console) {
     const playerMove = line.trim().toUpperCase() as RPSMove;
     const aiPred = ai.getNextMovePrediction();
@@ -25,10 +27,14 @@ for await (const line of console) {
     const aI = moves.indexOf(aiMove);
     if (pI === aI) {
         console.log('Tie!');
+        score[1]++;
     } else if ((3+aI-pI)%3 === 1){
         console.log('AI wins!');
+        score[2]++;
     } else {
         console.log('Player wins!');
+        score[0]++;
     }
+    process.stdout.write(`Wins/Ties/Losses: ${score[0]}/${score[1]}/${score[2]}\n`);
     process.stdout.write('Enter your move [R, P, S]: ');
 }
