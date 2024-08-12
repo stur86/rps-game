023bf72f1b7ca33950d6a6f1603ccb71257bb657
Simone <simonesturniolo@gmail.com>
2024-08-12 14:41:27 +0100

    Fixed bug in CLI game

--- game.ts
+++ game.ts
@@ -3,6 +3,7 @@ import { RPSLinearNGramCombo } from "./src/linear";
 
 // Use the command argument to determine the number of n-grams to use
 const n = parseInt(process.argv[2]) || 3;
+const debug = process.argv[3] === 'debug';
 const ai = new RPSLinearNGramCombo(n);
 
 const moves = [RPSMove.ROCK, RPSMove.PAPER, RPSMove.SCISSORS];
@@ -10,9 +11,15 @@ const moves = [RPSMove.ROCK, RPSMove.PAPER, RPSMove.SCISSORS];
 process.stdout.write('Enter your move [R, P, S]: ');
 for await (const line of console) {
     const playerMove = line.trim().toUpperCase() as RPSMove;
-    const aiMove = moves[(ai.getNextMovePrediction().index+1)%3];
+    const aiPred = ai.getNextMovePrediction();
+    if (debug) {
+        console.log("Prediction matrix:\n", aiPred.matrix);
+        console.log("Probabilities:\n", aiPred.probabilities);
+        console.log("Weights:\n", ai.x);
+    }
+    const aiMove = moves[(aiPred.index+1)%3];
     console.log(`AI plays: ${aiMove}`);
-    ai.updateWeights({player: aiMove, opponent: playerMove});
+    ai.updateWeights({player: playerMove, opponent: aiMove});
     // Who won?
     const pI = moves.indexOf(playerMove);
     const aI = moves.indexOf(aiMove);
@@ -23,7 +30,5 @@ for await (const line of console) {
     } else {
         console.log('Player wins!');
     }
-    console.log(ai.x);
-    console.log(ai.getNextMovePrediction().matrix);
     process.stdout.write('Enter your move [R, P, S]: ');
 }
