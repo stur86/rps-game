7d9be8bcf38387dda96a1ab87cdb8a08930fb8c3
Simone <simonesturniolo@gmail.com>
2024-08-13 17:19:29 +0100

    feat: Add playFunction to Hand component for handling user moves

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,13 @@
 import Hand from "./Hand"
+import { RPSMove } from "../ai/ngram";
 
 export default function Game() {
+
+    function playFunction(m: RPSMove) {
+        console.log(m);
+    }
+
     return (<div className="rps-game">
-        <Hand />
+        <Hand playFunction={playFunction} />
     </div>);
 }
--- src/components/Hand.tsx
+++ src/components/Hand.tsx
@@ -3,10 +3,10 @@ import Card from "./Card";
 
 const moves = [RPSMove.ROCK, RPSMove.PAPER, RPSMove.SCISSORS];
 
-export default function Hand() {
+export default function Hand(props: { playFunction: (m: RPSMove) => void }) {
     return (<div className="rps-hand">
         {
-            moves.map((move) => <div onClick={() => { console.log(move); }} key={move}><Card symbol={move} /></div>)
+            moves.map((move) => <div onClick={() => { props.playFunction(move); }} key={move}><Card symbol={move} /></div>)
         }
     </div>);
 }
