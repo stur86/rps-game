2b65d39e6c22e9692e6a571ea49a8a243e60fa55
Simone <simonesturniolo@gmail.com>
2024-08-14 08:01:02 +0100

    Added score display

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,25 @@
 import Hand from "./Hand"
-import { RPSMove } from "../ai/ngram";
+import { type RPSTurn, RPSMove } from "../ai/ngram";
+import GameLogic, { type Score } from "../game";
+import { useEffect, useState } from "react";
+
+let game = new GameLogic(3);
+
+function ScoreDisplay(props: { W: number, T: number, L: number }) {
+    return (<div className="rps-score">
+        {props.W}W/{props.T}T/{props.L}L
+    </div>);
+}
 
 export default function Game() {
+    const [lastTurn, setLastTurn] = useState<RPSTurn | null>(null);
 
     function playFunction(m: RPSMove) {
         console.log(m);
     }
 
     return (<div className="rps-game">
+        <ScoreDisplay W={game.score.W} T={game.score.T} L={game.score.L} />
         <Hand playFunction={playFunction} />
     </div>);
 }
