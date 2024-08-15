80cd1998cca269603bcab93b3ef71aac3a2cd6d2
Simone <simonesturniolo@gmail.com>
2024-08-15 06:46:20 +0100

    feat: Add TurnDisplay component to show the outcome of each turn

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,6 +2,7 @@ import Hand from "./Hand"
 import { RPSMove } from "../ai/ngram";
 import GameLogic, { type Score } from "../game";
 import { useEffect, useState } from "react";
+import Card from "./Card";
 
 let game = new GameLogic(3);
 
@@ -17,6 +18,36 @@ function ScoreDisplay(props: { W: number, T: number, L: number }) {
     </div>);
 }
 
+function TurnDisplay(props: { turn: TurnInfo|null }) {
+
+    let resultMsg = "";
+    if (props.turn !== null) {
+        if (props.turn.result === 1) {
+            resultMsg = "You win!";
+        } else if (props.turn.result === 0) {
+            resultMsg = "It's a tie!";
+        } else {
+            resultMsg = "You lose!";
+        }
+    }
+
+    return (<div className="rps-turn">
+        <div className="rps-turn-moves">
+            {
+                props.turn === null ? null : 
+                <div className="rps-move"><Card symbol={props.turn.player}/>Player</div>
+            }
+            {
+                props.turn === null ? null : 
+                <div className="rps-move"><Card symbol={props.turn.ai}/>AI</div>
+            }
+        </div>
+        <div className="rps-turn-outcome">
+            {resultMsg}
+        </div>
+    </div>);
+}
+
 export default function Game() {
     const [lastTurn, setLastTurn] = useState<TurnInfo | null>(null);
 
@@ -27,6 +58,7 @@ export default function Game() {
 
     return (<div className="rps-game">
         <ScoreDisplay W={game.score.W} T={game.score.T} L={game.score.L} />
+        <TurnDisplay turn={lastTurn} />
         <Hand playFunction={playFunction} />
     </div>);
 }
